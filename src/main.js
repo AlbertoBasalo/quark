1dac61c8b251c3ae6c65416e97d50e41362b000a
Alberto Basalo <albertobasalo@hotmail.com>
2021-01-05 13:57:57 +0100

    test: Add and configure jest Fixes #13

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+export { sayHello, sum };
 function sayHello(yourName) {
   console.log(`Hello ${yourName}`);
+}
+function sum(a, b) {
+  return a + b;
 }
 const myName = 'Quark';
 sayHello(myName);
---
+++ src/main.spec.js
@@ -0,0 +1,14 @@
+import { sayHello, sum } from './main';
+
+test('say hello function exists', () => {
+  expect(sayHello).toBeDefined();
+});
+
+describe('GIVEN the sum function', () => {
+  const sut = sum;
+  it('THEN should sums numbers', () => {
+    const actual = sut(1, 2);
+    const expected = 3;
+    expect(actual).toBe(expected);
+  });
+});
