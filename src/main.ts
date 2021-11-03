57c9274d95e487c76f2baea8735c9c8f2adaac39
Alberto Basalo <albertobasalo@hotmail.com>
2021-11-03 17:20:41 +0100

    chore: npm update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ export { sayHello, sum };
 function sayHello(yourName: string): void {
   console.log(`Hello ${yourName}`);
 }
-function sum(a: number, b: number): number {
+function sum( a: number, b: number ): number {
+  if ( a == 0 ) {
+    return b;
+  }
   return a + b;
 }
 const myName = 'Quark';
