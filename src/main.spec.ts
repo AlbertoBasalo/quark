7f24c497d70e3f8a11e2a37b014e034f1062cf64
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-26 11:38:51 +0100

    chore: lint

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,12 +1,14 @@
-import { sayHello, sum } from './main';
+import { sayHello, sum } from "./main";
 
-test('sayHello function exists', () => {
-  expect(sayHello).toBeDefined();
+describe("hello", () => {
+  it("sayHello function exists", () => {
+    expect(sayHello).toBeDefined();
+  });
 });
 
-describe('The sum function', () => {
+describe("the sum function", () => {
   const sut = sum;
-  it('should sum numbers', () => {
+  it("should sum numbers", () => {
     const actual = sut(1, 2);
     const expected = 3;
     expect(actual).toBe(expected);
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ export { sayHello, sum };
 function sayHello(yourName: string): void {
   console.log(`Hello ${yourName}`);
 }
-function sum( a: number, b: number ): number {
-  if ( a == 0 ) {
+function sum(a: number, b: number): number {
+  if (a == 0) {
     return b;
   }
   return a + b;
 }
-const myName = 'Quark';
+const myName = "Quark";
 sayHello(myName);
